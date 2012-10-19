7a2b3519fbb027823504bbd1c184a474e0307bbd
Gregory Grillone <greg.grillone@gmail.com>
2012-10-19 17:35:31 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VtBrewTour::Application.config.secret_token = 'e77cf03aca92d76f475acfe7598874c66ecc41147f38d6faec8c74a65f26158a5a09171020e0231edb995aa6cda1b87d6fb88bbcdb4d701aa9a50a58693af4e7'
